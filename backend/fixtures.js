a5dd91295af28fe86dd7f7f6479f052bb8c51ab4
Mgoumane <goumane.mahmoudou@gmail.com>
2021-03-31 14:38:32 +0200

    fixtures created

---
+++ backend/fixtures.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const productsModel = require("");
+
+const products = [
+    {
+        nameOfProduct: "Peugeot 3008",
+        description:" La Peugeot 3008 est un SUV compact du constructeur automobile français Peugeot",
+        locationOfProduct: "Paris",
+        postedBy:"Mamoudou ",
+        dateOfPost: 2016/01/15,
+        priceOfProduct: 9000 ,
+        imageOfProduct: "",
+    },
+    {
+        nameOfProduct: "Peugeot 208",
+        description:" La Peugeot 208 I est une automobile conçue par le constructeur Peugeot pour succéder à la 207",
+        locationOfProduct: "Marseile",
+        postedBy:"Mamoudou ",
+        dateOfPost: 2011/12/03,
+        priceOfProduct: 6000,
+        imageOfProduct: "",
+    },
+    {
+        nameOfProduct: "Citroen ds5",
+        description:" La DS 5 est une berline de luxe à 5 portes du constructeur automobile français DS Automobiles",
+        locationOfProduct: "Lyon ",
+        postedBy:"Mamoudou ",
+        dateOfPost: 2017/07/12,
+        priceOfProduct: 5000 ,
+        imageOfProduct: "",
+    },
+   
+]
