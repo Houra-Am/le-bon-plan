06be0f2e69b502e838fb1b6a4cf0af8bcb9353e5
unknown <samassaadama@hotmail.fr>
2021-03-31 12:34:36 +0200

    productsModel updated/upload started

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,18 @@ const express = require ("express");
 const bodyParser = require("body-parser");
 const bcryptjs = require("bcryptjs");
 const mongoose = require ("mongoose");
+const multer = require ("multer");
 const cors = require ("cors");
 const jwt = require("jsonwebtoken");
 const config = require("./config");
 //const productsModel = require ("./models/products");
 //const port = 8000;
 const app = express();
-
+const upload = multer({dest: "public/images"});
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static("public"));
 
 app.listen(config.port, () => {
     console.log("server connected on port " + config.port);
--- backend/models/products.js
+++ backend/models/products.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const productsSchema = new Mongoose.Schema({
-    nameOfProduct: String,
-    description: String,
-    locationOfProduct: String,
-    postedBy: String,
-    dateOfPost: Date
+    nameOfProduct: {Type: String, required = true},
+    description: {Type: String, required = true},
+    locationOfProduct: {Type: String, required = true},
+    postedBy: {Type: String, required = true},
+    dateOfPost: {Type: Date, required = true},
+    priceOfProduct: {Type: Number, required = true}
 });
 
 const productsModel = mongoose.model("products", productsSchema);
